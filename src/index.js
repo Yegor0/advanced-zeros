e02b9331b7845b56d8a30d4bce619da8a9b792e8
Yegor Kuzin <yahor2000@gmail.com>
2019-03-24 18:53:39 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+let k = 0;
+let array = new Array();
+let n = 0;
+let i = 2;
+const baseNumber = base;
+while(i <= baseNumber) {
+  if (base % i == 0) {
+    a = i;
+    n++;  
+    base /= i;
+  } else {
+    if(n != 0) {
+      array[k] = CountZeros(number, a, n);
+      k++;
+    }  
+    i++;
+    n = 0; 
+  }
+}
+
+return Math.min(...array);
+
+
+function CountZeros(number, a, n) {
+  let sum = 0;
+  for(let i = 1; Math.pow(a, i) < number; i++) {
+    sum += Math.floor(number / Math.pow(a, i));
+  }
+  return Math.floor(sum / n);
+  }
 }
